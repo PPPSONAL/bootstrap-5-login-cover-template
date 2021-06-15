89c863855fa78fef9b26f2db0154d09d54cc234b
PPPSONAL <sonalpanchal144@gmail.com>
2021-06-15 13:25:38 +0530

    Create Pipe-Serach.ts

---
+++ Pipe-Serach.ts
@@ -0,0 +1,42 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'cutomSearchFilter'
+})
+export class CutomSearchFilterPipe implements PipeTransform {
+  transform(items: any, filter: any, defaultFilter: boolean): any {
+    if (!filter){
+      return items;
+    }
+
+    if (!Array.isArray(items)){
+      return items;
+    }
+
+    if (filter && Array.isArray(items)) {
+      let filterKeys = Object.keys(filter);
+        
+      if (defaultFilter) {
+        return items.filter(item =>
+            filterKeys.reduce((x, keyName) =>
+                (x && new RegExp(filter[keyName], 'gi').test(item[keyName])) || filter[keyName] == "", true));
+      }
+      else {
+        return items.filter(item => {
+          return filterKeys.some((keyName) => {
+            return new RegExp(filter[keyName], 'gi').test(item[keyName]) || filter[keyName] == "";
+          });
+        });
+      }
+    }
+  }
+}
+
+////
+
+In Components
+
+
+<div *ngFor="let item of examList | cutomSearchFilter: {examTitle: searchText, incomeLimitQualification: searchText, examStartDate: searchText, examStartTime: searchText, examRoom: searchText , examDay: searchText} : false; let i=index;"  >
+
+</div>
